4a608cbaebfc57b1501a0093bb5040913c950add
Adi <111001628+xndadelin@users.noreply.github.com>
2025-03-09 07:48:48 +0000

    FINISH

--- main.py
+++ main.py
@@ -38,41 +38,35 @@ class Presentation(Widget):
 ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗     
 ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║     
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝     
-This password manager is a simple application that allows you to store your passwords in a secure way.
+This password manager is a simple local application that allows you to store your passwords in a secure way.
 It uses a master phrase to encrypt and decrypt your passwords.
 It also allows you to generate random passwords.
 It uses the Fernet symmetric encryption algorithm to encrypt and decrypt your passwords.
 It is a simple terminal application that is easy to use.
 The master phrase and Fernet key are stored in a .env file.
-The encrypted passwords are stored in a .txt file.
+The encrypted passwords are stored in a SQLite3 database.
 TERMINAL CRAFT
         """, id="ascii_art", classes="static_header")
 
 class Choice(Widget):
     def compose(self) -> ComposeResult:
         yield Static("""
-
-
-             *****************                                                                      
-          =*************************                                                         **     
-         =*******+***********************                                                 ****      
-        =*******   ***** -********************                                       ********       
-       =+******.   ****   *****  =****************                             *************        
-      ==*******    .+*   -****    **************************         **********************         
-     ==*******.          ***+  *  ****     ************************************     **************  
-    ==*******+   ****   +**+ .**  **=  -*****.  ***********************=***  ** -* .************    
-    =+*******=  ****+  .**+      .**  .******   **  *******: .*** +****  **: -*: .++ +********      
-   ==***************:  **-  ***  +*+  *******  .   *******  +:.**  ****. ***  ** :*: ******         
-  ==+*************************   **+  ******:    +*******: ******  ***** .**  **- :******           
-  ==          **********************     **=  *+  *******. +*****  ******    *********              
- ===                  ****************=-**+  ***  .*******  *****-      ***********                 
- ===                         *******************- .*******+    :****************                    
- ==                                *****************************************                        
-===                                        ****************************                             
-===                                                                                                 
-===                                                                                                 
-
-                                                                                                                                                                                                                     
+@@@@@@@@@@BGBGP55YYYYYYY555PGGB#&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@P?YYYYYYYYYYYYYYYYYYYYYY55PGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BG&@@@@@
+@@@@@@@@5?YYYYYY~:7YYYYYJYYYYYYYYYYYYYYY5PB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BP5YY#@@@@@@
+@@@@@@@57YYYYYY7   JYYY~.:JYYYY?7JYYYYYYYYYYY5GB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGP5YYYYY5&@@@@@@@
+@@@@@@57YYYYYYY:   7JJ^  ^YYYJ^  :YYYYYYJYYYYYYYYY5PPGB##&&&&@@@@@@&&&&&##BGGP55YYJYYYYYYY5&@&&&&&&&
+@@@@@57JYYYYYY!      .   ?YYJ..!. JYYJ~...:JYYJYYYYYYYYYYYYY55555555YYYYYYYYYYJ!~^:^YYYYYYY5555YY5G&
+@@@@G7JYYYYYY?   !J?!   !YY?..7J..YY7  ^?7~JY~ ^YYYYYYYYYYYYYYYYYYYYYYJ7JYY~~Y7 ~?: JYYYYYYYYYY5B&@@
+@@@B7?YYYYYYY7.^?YY5?  :YY!      ^YJ. ^YYYYYY: :Y7^:JYYYYYJ~^~JY?.7YYY? ^YY~ !5^ ^!~.7YYYYYY5G#@@@@@
+@@&?7YYYYYYYYYYYYYYY!:~JY! :??~  ?5!  JYYYYYJ. .: ^?YYYYY? :!~!Y7 :YYYY~ ?5? :YJ.^7~:?YYY5P#@@@@@@@@
+@@57JY5555YYYYYYYYYYYYYYY??YYY: ~YY7  7JYYYY^ .. ^YYYYYYY~ ?5YYYJ. JYYYY..?7 :YY?!!?YY5P#&@@@@@@@@@@
+@#7J&&&&&&&&&&##BGGP55YYYYYYYYJJYYYY~. .~YJ: :J?  !YYYYYY~ ~YYYYY^ 7J?7?J!^^~JYYYYY5G#&@@@@@@@@@@@@@
+@Y7P@@@@@@@@@@@@@@@@@@&##BGP55YYYYYYYYJJYY?^7YYY: :YYYYYYJ: ^~^!Y7.:^^~7YYYYYYY5PB&@@@@@@@@@@@@@@@@@
+#?7#@@@@@@@@@@@@@@@@@@@@@@@@@&&#BGP55YYYYYYYYYYYJ7?YYYYYYYY7~~!JYYYYYYYYYYY5PB#&@@@@@@@@@@@@@@@@@@@@
+P7?&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&#BGGP555YYYYYYYYYYYYYYYYYYYYYY55PG#&@@@@@@@@@@@@@@@@@@@@@@@@@
+J7J@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&###BBBBGGGGGGGGBB##&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+?7J@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                                                                                                                                                                                     
 """, id="ascii_art")
     
         with Vertical(id="buttons_container"):
